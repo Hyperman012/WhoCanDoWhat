45d807f1077b17c5b0aceaca9d469d595ad7be06
Adam Whaley <abwn83@gmail.com>
2025-12-15 18:20:34 -0600

    other fixes

--- src/LegacyAccess.test.tsx
+++ src/LegacyAccess.test.tsx
@@ -4,8 +4,6 @@ import { LegacyAccess } from './LegacyAccess'
 describe('Who Can Do What?', () => {
   it('foo', () => {
     render(<LegacyAccess />)
-
-    // Intentionally wrong, tiny failure
     expect(screen.getByTestId('perm-users.read')).toHaveAttribute('data-status','fixme')
   })
 })
